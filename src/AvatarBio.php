aad156f9d822336587841571c12fb61d0a9825d7
Matt Grandy <matt@clientexec.com>
2016-11-24 23:29:56 -0500

    Use www.avatar.bio instead of just avatar.bio as heroku does not support SSL on just the domain

--- src/AvatarBio.php
+++ src/AvatarBio.php
@@ -7,7 +7,7 @@ use Egulias\EmailValidator\Validation\RFCValidation;
 
 class AvatarBio {
 
-    private $url             = 'https://avatar.bio/avatar/';
+    private $url             = 'https://www.avatar.bio/avatar/';
     private $backGroundColor = 'DAF1FF';
     private $textColor       = '555555';
     private $text            = '';
